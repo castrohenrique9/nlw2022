7e79b2c95271748ff57993ac21719060aa69874e
Castro Souza <castrohenrique9@gmail.com>
2022-09-12 21:57:53 -0300

    request with json format

--- src/server.mjs
+++ src/server.mjs
@@ -3,7 +3,11 @@ import express from 'express'
 const app = express()
 
 app.get('/ads', (request, response) => {
-    response.send('Access ads!')
+    response.json([
+        { id: 1, name: 'Ads 1' },
+        { id: 2, name: 'Ads 3' },
+        { id: 3, name: 'Ads 3' }
+    ])
 })
 
 // localhost:3333/ads
